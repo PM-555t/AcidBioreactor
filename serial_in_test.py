27ee50e068d43ee072cf74fb75a381a1ca39c90d
PMarsh <paul.marsh@pnnl.gov>
2024-08-29 08:57:05 -0700

    initial commit

---
+++ serial_in_test.py
@@ -0,0 +1,10 @@
+import time
+import serial
+
+ser = serial.Serial(port='/dev/ttyACM0',baudrate=9600,parity=serial.PARITY_NONE,stopbits=serial.STOPBITS_ONE,bytesize=serial.EIGHTBITS,timeout=1)
+
+while True:
+    x=ser.readline()
+    b=open("20240828_3.txt","a")
+    b.write(str(x))
+    b.close()
