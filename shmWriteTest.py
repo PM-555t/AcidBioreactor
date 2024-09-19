0dbad80d2969ba9a7b85a9f97b41a47922b31572
PMarsh <paul.marsh@pnnl.gov>
2024-09-19 07:53:27 -0700

    Updating gitignore and removing cached links

--- shmWriteTest.py
+++
@@ -1,9 +0,0 @@
-# task executed in a child process
-def task(shared_mem):
-    # write some string data to the shared memory
-    shared_mem.buf[:24] = b'Hello from child process'
-    # close as no longer needed
-    shared_mem.close()
-    
-checkStr = 'This is my longer total string' + str(7)
-print(len(checkStr))
